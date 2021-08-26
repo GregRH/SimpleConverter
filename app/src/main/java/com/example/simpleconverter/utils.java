e4dca6b1ef8dde5a042b1a7ad844562c097d8185
gregoryhoag <gryanhoag@gmail.com>
2021-08-26 13:42:13 -0400

    Reduces even fractions

--- app/src/main/java/com/example/simpleconverter/utils.java
+++ app/src/main/java/com/example/simpleconverter/utils.java
@@ -1,10 +1,11 @@
 package com.example.simpleconverter;
 import java.lang.Math;
 public class utils {
-    public static String decToFrac(double x, int d) {
+    public static String decToFrac(double input, int d) {
         //pass it a number in decimal and it will convert it to the nearest fraction with d as the denominator
         //d > 0
-        String r = Integer.toString((int) x) + "";
+        String r = Integer.toString((int) input);
+        double x = input;
         double err = 1 / (d * 2.0); //err is halfway between units
         x = x - (int) x;
         int i = 0;
@@ -12,14 +13,52 @@ public class utils {
             i++;
             x = x - (1.0 / d);
         }
-        r = r + " " + Integer.toString(i) + "/" + Integer.toString(d);
+        String reduced = reduceFrac(i,d);
+        if(reduced.toCharArray()[0]=='1'&&reduced.toCharArray()[2]=='1'&&reduced.length()==3)
+            //if 1/1 add one
+            r=Integer.toString((int) input+1);
+        else if(reduced.toCharArray()[0]!='0')
+            //if not 0/x add the fraction
+            r = r + " " + reduced;
         return r;
     }
-    public static String reducFrac(String frac){
-        return "";
+    public static String reduceFrac(String frac){
+        int numerator=0;
+        int denominator=0;
+        int slash_pos=0;
+        for(int i = 0; i<frac.length();i++){
+            //find numerator
+            if(frac.toCharArray()[i]=='/'){
+                slash_pos=i;
+                break;
+            }
+            numerator*=10;
+            numerator+=frac.toCharArray()[i]-48;
+        }
+        for(int i = slash_pos+1; i<frac.length();i++){
+            //find denominator
+            denominator*=10;
+            denominator+=frac.toCharArray()[i]-48;
+        }
+        return reduceFrac(numerator,denominator);
     }
     public static String reduceFrac(int numerator, int denominator){
-        return "";
+        int x=2;
+        try {
+            while((numerator&1)==0&&(denominator&1)==0){//Even numbers only
+                //x=2;
+                numerator=numerator/x;
+                denominator=denominator/x;
+            }
+        	/*while(denominator%numerator==0 && numerator>1&&x!=1){
+        		x=denominator/numerator;
+        		numerator=numerator/x;
+        		denominator=denominator/x;
+        	}*/
+        }catch (Exception e) {
+            //you probably divided by 0
+        }
+        return Integer.toString(numerator)+"/"+Integer.toString(denominator);
     }
     public static boolean isRightTriangle(Triangle a){
         //Check sides, valid if pythagorean theorem
@@ -45,12 +84,12 @@ public class utils {
             a.setAngleA(Math.toDegrees(Math.acos (a.getSide_b()/a.getSide_c())));
             a.setAngleB(Math.toDegrees(Math.asin (a.getSide_b()/a.getSide_c())));
         }
-        if (a.getAngleA()>0 && a.getAngleB()>0){
+       /* if (a.getAngleA()>0 && a.getAngleB()>0){
             //all angles calculated
         }
         else {
             //not enough information
-        }
+        }*/
     }
     public static void calculateSides(Triangle a){
 
